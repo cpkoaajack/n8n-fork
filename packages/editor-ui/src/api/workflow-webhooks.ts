f9fbd716731f0dae14074a5e3d1e6f2842423436
Milorad Filipovic <milorad@n8n.io>
2022-07-08 17:41:01 +0200

    Using a temporary webhook URL for testing onboarding call prompts

--- packages/editor-ui/src/api/workflow-webhooks.ts
+++ packages/editor-ui/src/api/workflow-webhooks.ts
@@ -2,8 +2,8 @@ import { IOnboardingCallPromptResponse, IUser } from "@/Interface";
 import { get } from "./helpers";
 
 // TODO: Replace this with production when the workflow is ready
-const N8N_WF_WEBHOOK_BASE_URL = 'https://internal.users.n8n.cloud/webhook-test';
-const N8N_ONBOARDING_CALL_PROMPT_WEBHOOK_PATH = '56f4e271-4e9f-4b38-b21e-0fa2d5113238';
+const N8N_WF_WEBHOOK_BASE_URL = 'https://milorad.app.n8n.cloud/webhook';
+const N8N_ONBOARDING_CALL_PROMPT_WEBHOOK_PATH = '/46a6a97e-dff8-43bb-8085-535a9455bb0a';
 
 export async function fetchNextOnboardingPrompt(instanceId: string, currentUer: IUser): Promise<IOnboardingCallPromptResponse> {
 	return await get(
